3cdf71000ecbaf92567af67f213badf163cdd6a2
JiwonShin <sarah95071626@gmail.com>
2017-05-12 23:06:05 +0900

    load modal placeholder

--- HyuJokbo/HyuJokbo/JokboUploadViewController.swift
+++ HyuJokbo/HyuJokbo/JokboUploadViewController.swift
@@ -8,15 +8,48 @@
 
 import UIKit
 
-class JokboUploadViewController: UIViewController {
+class JokboUploadViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var TitleTextView: UITextView!
     @IBOutlet weak var ProfessorTextView: UITextView!
     @IBOutlet weak var ContentTextView: UITextView!
     
+    var TitlePlaceholderLabel: UILabel!
+    var ProfessorPlaceholderLabel: UILabel!
+    var ContentPlaceholderLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.TitleTextView?.becomeFirstResponder()
+        
+        self.TitleTextView.delegate = self
+        self.ProfessorTextView.delegate = self
+        self.ContentTextView.delegate = self
+        
+        self.TitleTextView.tag = 0
+        self.ProfessorTextView.tag = 1
+        self.ContentTextView.tag = 2
+        
+        /*TitlePlaceholderLabel = UILabel()
+        TitlePlaceholderLabel.text = "수업명"
+        TitlePlaceholderLabel.textColor = UIColor.lightGray
+        TitlePlaceholderLabel.font = UIFont.italicSystemFont(ofSize: (TitleTextView.font?.pointSize)!)
+        TitlePlaceholderLabel.sizeToFit()
+        self.TitleTextView?.addSubview(TitlePlaceholderLabel)
+        TitlePlaceholderLabel.frame.origin = CGPoint(x: 5, y: (TitleTextView.font?.pointSize)! / 2)
+        TitlePlaceholderLabel.textColor = UIColor.lightGray
+        TitlePlaceholderLabel.isHidden = !TitleTextView.text.isEmpty
+        
+        
+        ProfessorPlaceholderLabel = UILabel()
+        ProfessorPlaceholderLabel.text = "수업명"
+        ProfessorPlaceholderLabel.textColor = UIColor.lightGray
+        ProfessorPlaceholderLabel.font = UIFont.italicSystemFont(ofSize: (ProfessorTextView.font?.pointSize)!)
+        ProfessorPlaceholderLabel.sizeToFit()
+        self.ProfessorTextView?.addSubview(ProfessorPlaceholderLabel)
+        ProfessorPlaceholderLabel.frame.origin = CGPoint(x: 5, y: (TitleTextView.font?.pointSize)! / 2)
+        ProfessorPlaceholderLabel.textColor = UIColor.lightGray
+        ProfessorPlaceholderLabel.isHidden = !ProfessorTextView.text.isEmpty*/
+        
         self.TitleTextView?.text = " 수업명"
         self.TitleTextView?.textColor = UIColor.lightGray
         self.ProfessorTextView?.text = " 교수님"
@@ -33,7 +66,7 @@ class JokboUploadViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         self.TitleTextView?.setContentOffset(CGPoint.zero, animated: false)
@@ -54,11 +87,17 @@ class JokboUploadViewController: UIViewController {
     
     func textViewDidEndEditing(_ textView: UITextView) {
         if textView.text.isEmpty {
-            textView.text = "Placeholder"
+            if textView.tag == 0 {
+                textView.text = " 수업명"
+            } else if textView.tag == 1 {
+                textView.text = " 교수님"
+            } else if textView.tag == 2 {
+                textView.text = " 여기를 눌러서 글을 작성할 수 있습니다."
+            }
             textView.textColor = UIColor.lightGray
         }
     }
-    /*
+        /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
