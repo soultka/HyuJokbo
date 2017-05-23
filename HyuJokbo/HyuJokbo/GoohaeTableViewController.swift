c6ead45483259d708669df3b6a40e58144b5cd11
PBW <qkrquddnr99@gmail.com>
2017-05-23 12:38:00 +0900

    GoohaeTab UI Complete(by copy & connecting @IBOutlet,@IBAction,segue)

--- HyuJokbo/HyuJokbo/GoohaeTableViewController.swift
+++ HyuJokbo/HyuJokbo/GoohaeTableViewController.swift
@@ -1,23 +1,76 @@
 //
-//  GohaeTableViewController.swift
-//  HyuJokbo
+//  GoohaeTableViewController.swift
+//  HyuGoohae
 //
 //  Created by ByoungWook Park on 2017. 5. 12..
 //  Copyright © 2017년 박한솔. All rights reserved.
 //
 
 import UIKit
+import FirebaseDatabase
 
-class GohaeTableViewController: UITableViewController {
+
+
+class GoohaeTableViewController: UITableViewController,GoohaeDownload {
+
+    var ref:FIRDatabaseReference?
+    var databaseHandle:FIRDatabaseHandle?
+    var GoohaesData = [[String:String]]()
+
+    //서치 버튼이 표시되었을 경우 1, 표시 안되어 있을 경우 0
+    static var searchPressedFlag = 0
+
+    //스크롤을 아래로 내리면 리프레쉬뷰가 나오도록하는 클래스
+    var refresh = UIRefreshControl()
+
+    //검색창 서브뷰
+    var searchSubView:UIView!
+
+
+    @IBAction func handleModalClose(segue: UIStoryboardSegue){
+
+
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //테이블 뷰의 왼쪽위 좌표를 CGPoint로 얻어옴
+        let topOfTableCGPoint = self.tableView.topAnchor.accessibilityActivationPoint
+        //서브뷰(검색창)의 CGSize를 얻어옴
+        let subviewCGSize = CGSize(width: self.view.frame.width, height: self.view.frame.height)
+        //얻어온 값을 기준으로 검색창 서브뷰 설정
+        searchSubView = SearchView(frame:CGRect(origin: topOfTableCGPoint, size: subviewCGSize))
+
+        self.tableView.addSubview(refresh)
+        //검색창 서브뷰 추가
+        self.view.addSubview(searchSubView)
+        //검색창 서브뷰 기본적으로 숨겨짐
+        self.searchSubView.isHidden = true
+
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+
+        ref = FIRDatabase.database().reference()
+        //Retrieve the posts and listen for changes
+
+        databaseHandle = ref?.child("Goohaes").observe(.childAdded, with: { (snapshot) in
+            //Take the value from the snapshot and added it to the GoohaesData array
+            let Goohae = snapshot.value as? [String:String]
+            if let actualGoohae = Goohae{
+                //Append the data to our Goohae array
+
+                self.GoohaesData.append(actualGoohae)
+                //reload the tableview
+                self.tableView.reloadData()
+            }
+
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,67 +82,115 @@ class GohaeTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        let rowCount = GoohaesData.count
+        return rowCount
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GoohaeCell", for: indexPath) as! GoohaeTableViewCell
+        print(GoohaesData)
+        var GoohaeDataShow = GoohaesData[indexPath.row]
+        //Goohaes로 부터 Goohae를 받아옴
+        guard let Goohae = GoohaeDataShow["GoohaeText"]  else {
+            return cell
+        }
+        if let subject = GoohaeDataShow["className"] {
+            cell.SubjectLabel?.text = subject
+        }
+
+        if let professor = GoohaeDataShow["professorName"] {
+            cell.ProfessorLabel?.text = professor
+        }
+
+        cell.downloadDelegate = self
+
 
         return cell
     }
-    */
+
+    //Download 버튼 클릭시 호출
+    func download() {
+        //to do list
+        //다운로드 만들것
+        print("Downloading...")
+    }
+
+    @IBAction func SearchBarButtonPressed(_ sender: Any) {
+
+        //검색창이 표시되었을 경우
+        if(GoohaeTableViewController.searchPressedFlag == 1)
+        {
+
+            self.tableView.reloadData()
+
+            self.searchSubView.isHidden = true
+
+            GoohaeTableViewController.searchPressedFlag = 0
+
+        }else {
+            //검색창이 표시안되있을경우
+            self.tableView.reloadData()
+
+            self.searchSubView.isHidden = false
+
+            GoohaeTableViewController.searchPressedFlag = 1
+
+        }
+
+
+    }
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
 
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
 
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+     }
+     */
 
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
 
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ HyuJokbo/HyuJokbo/GoohaeUploadTableViewController.swift
@@ -0,0 +1,105 @@
+//
+//  GoohaeUploadViewController.swift
+//  HyuGoohae
+//
+//  Created by Jiwon Shin on 2017. 5. 12..
+//  Copyright © 2017년 박한솔. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class GoohaeUploadViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var TitleTextView: UITextView!
+    @IBOutlet weak var ProfessorTextView: UITextView!
+    @IBOutlet weak var ContentTextView: UITextView!
+
+    var ref: FIRDatabaseReference?
+
+    var TitlePlaceholderLabel: UILabel!
+    var ProfessorPlaceholderLabel: UILabel!
+    var ContentPlaceholderLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ref = FIRDatabase.database().reference()
+
+        self.TitleTextView.delegate = self
+        self.ProfessorTextView.delegate = self
+        self.ContentTextView.delegate = self
+
+        self.TitleTextView.tag = 0
+        self.ProfessorTextView.tag = 1
+        self.ContentTextView.tag = 2
+
+        self.TitleTextView?.text = " 수업명"
+        self.TitleTextView?.textColor = UIColor.lightGray
+        self.ProfessorTextView?.text = " 교수님"
+        self.ProfessorTextView?.textColor = UIColor.lightGray
+        self.ContentTextView?.text = " 여기를 눌러서 글을 작성할 수 있습니다."
+        self.ContentTextView?.textColor = UIColor.lightGray
+
+        self.TitleTextView?.layer.borderWidth = 0.5
+        self.TitleTextView?.layer.borderColor = UIColor.lightGray.cgColor
+        self.ProfessorTextView?.layer.borderWidth = 0.5
+        self.ProfessorTextView?.layer.borderColor = UIColor.lightGray.cgColor
+        self.ContentTextView?.layer.borderWidth = 0.5
+        self.ContentTextView?.layer.borderColor = UIColor.lightGray.cgColor
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.TitleTextView?.setContentOffset(CGPoint.zero, animated: false)
+        self.ProfessorTextView?.setContentOffset(CGPoint.zero, animated: false)
+    }
+
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            if textView.tag == 0 {
+                textView.text = " 수업명"
+            } else if textView.tag == 1 {
+                textView.text = " 교수님"
+            } else if textView.tag == 2 {
+                textView.text = " 여기를 눌러서 글을 작성할 수 있습니다."
+            }
+            textView.textColor = UIColor.lightGray
+        }
+    }
+
+    @IBAction func addGoohae(_ sender: Any) {
+        // TODO: post the Goohae to firebase
+        let curRef = ref?.child("Goohaes").childByAutoId()
+        curRef?.child("className").setValue(TitleTextView.text)
+        curRef?.child("professorName").setValue(ProfessorTextView.text)
+        curRef?.child("GoohaeText").setValue(ContentTextView.text)
+        // Dismiss the popover
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
