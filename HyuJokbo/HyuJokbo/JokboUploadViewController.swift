8d43c43afe5853db82915f3259cb16bfad87c681
PBW <qkrquddnr99@gmail.com>
2017-06-01 15:12:57 +0900

    upload-photo finish

--- HyuJokbo/HyuJokbo/JokboUploadViewController.swift
+++ HyuJokbo/HyuJokbo/JokboUploadViewController.swift
@@ -159,19 +159,21 @@ class JokboUploadViewController: UIViewController, UITextViewDelegate, UIImagePi
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.allowsEditing = false
-        picker.
         present(picker, animated: true, completion: nil)
 
     }
-    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
         var selectedImageFromPicker:UIImage?
-
-        if let originalimage = info[UIImagePickerControllerOriginalImage] as? UIImage{
-            selectedImageFromPicker = originalimage
+        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage{
+                selectedImageFromPicker = editedImage
+        }
+        if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            selectedImageFromPicker = originalImage
         }
         if let s = selectedImageFromPicker{
             self.selectedImage = s
         }
+        dismiss(animated: true, completion: nil)
 
     }
     func addPhoto(key:String){
